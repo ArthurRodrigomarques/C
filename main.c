f39d4399c3fe2fb2cdffcae8b7d25a9593e6d5d8
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2024-10-29 13:37:21 -0300

    add
    add

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main() {
+    setlocale(LC_ALL, "Portuguese_Brazil.1252");
+
+    char nome[30];
+
+    scant("%s", nome);
+    // qets();
+
+    int n = 0, x = 1;
     
+    printf("Olá mundo!\n");
+
+    return 0;
 }
